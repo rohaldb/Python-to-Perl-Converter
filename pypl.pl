97cfb3e47d4f76510640ad68614f39ef140f1532
rohaldbUni <z5019999@ad.unsw.edu.au>
2017-09-17 15:58:55 +1000

    variable ass and print

--- pypl.pl
+++ pypl.pl
@@ -1,33 +1,43 @@
 #!/usr/bin/perl -w
 
-# Starting point for COMP[29]041 assignment 1
-# http://www.cse.unsw.edu.au/~cs2041/assignments/pypl
-# written by andrewt@unsw.edu.au September 2017
+# written by ben rohald 2017
 
 while ($line = <>) {
     if ($line =~ /^#!/ && $. == 1) {
 
         # translate #! line
-
         print "#!/usr/bin/perl -w\n";
 
     } elsif ($line =~ /^\s*(#|$)/) {
 
         # Blank & comment lines can be passed unchanged
-
         print $line;
 
-    } elsif ($line =~ /^\s*print\("(.*)"\)$/) {
+    } elsif ($line =~ /^\s*print\(("*)(.*)"*\)$/) {
+        $print_content = $2;
+        if ($1) {
+            print "print \"$2\\n\";\n";
+        } else {
+            #we are printing a variable
+            foreach $var (@variables) {
+                $print_content =~ s/$var/\$$var/g;
+            }
+            print "print \"$print_content\\n\";\n";
+        }
 
-        # Python's print outputs a new-line character by default
-        # so we need to add it explicitly to the Perl print statement
-
-        print "print \"$1\\n\";\n";
-
+    } elsif ($line =~ /^\s*(.*)=(.*)/) {
+        $lhs = $1;
+        $rhs = $2;
+        $lhs =~ s/ *//g;
+        push @variables, $lhs;
+        foreach $var (@variables) {
+            $rhs =~ s/$var/\$$var/g;
+        }
+        # variable assignment
+        print "\$$lhs=$rhs;\n";
     } else {
 
         # Lines we can't translate are turned into comments
-
         print "#$line\n";
     }
 }
